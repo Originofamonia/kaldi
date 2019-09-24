00840fd55b3d8653c9915e3ec25d91510c43c39e
edward <anqiyuanms@hotmail.com>
2019-09-23 21:23:27 -0400

    1. changed cuda-10.0 path 2. changed queue.pl to run.pl 3. changed timit dataset's path

--- egs/timit/s5/path.sh
+++ egs/timit/s5/path.sh
@@ -5,4 +5,4 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/irstlm/b
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
 
-
+# export LD_LIBRARY_PATH=/usr/local/cuda-10.0/targets/x86_64-linux/lib/stubs${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
